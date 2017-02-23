b528b93cfa0e685982f6f3468ae492a533598b7c
kjwook <cmdhema@gmail.com>
2017-02-23 10:50:26 +0900

    Add setPresenter function

--- app/src/main/java/kjw/kr/bunobuno/BaseView.java
+++ app/src/main/java/kjw/kr/bunobuno/BaseView.java
@@ -4,6 +4,7 @@ package kjw.kr.bunobuno;
  * Created by kjwook on 2017. 1. 20..
  */
 
-public interface BaseView {
+public interface BaseView<T> {
+    void setPresenter(T presenter);
     void showSuccessfullySavedMessage();
 }
